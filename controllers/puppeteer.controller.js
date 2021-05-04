4f0344e706fd1d8200666a9fa5b4bd6ed895b881
Mason <31558373+slioplark@users.noreply.github.com>
2021-05-04 17:57:29 +0800

    feat: use clip

--- controllers/puppeteer.controller.js
+++ controllers/puppeteer.controller.js
@@ -5,10 +5,15 @@ const getScreenshot = async (url = 'https://example.com') => {
 
   const page = await browser.newPage()
   await page.goto(url, { waitUntil: 'networkidle0' })
-  await page.setViewport({ width: 1024, height: 1024 })
   const buffer = await page.screenshot({
     encoding: 'binary',
     omitBackground: true,
+    clip: {
+      x: 0,
+      y: 0,
+      width: 800,
+      height: 1024,
+    },
   })
 
   await browser.close()
