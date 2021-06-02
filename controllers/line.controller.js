e9b7975ddc592c1e8d0715d017fd66c1154fab81
Mason <jeailfy9520@outlook.com>
2021-06-02 22:46:36 +0800

    feat: push message

--- controllers/line.controller.js
+++ controllers/line.controller.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const { v4: uuidv4 } = require('uuid')
 const line = require('@line/bot-sdk')
 const dayjs = require('dayjs')
+const axois = require('axios')
 const admin = require('firebase-admin')
 const serviceAccount = require('../serviceAccountKey.json')
 const puppeteerController = require('./puppeteer.controller')
@@ -9,7 +10,7 @@ const puppeteerController = require('./puppeteer.controller')
 const {
   DATABASE_URL,
   STORAGE_BUCKET,
-  LINE_NOTIFY_NAME,
+  LINE_USER_ID,
   LINE_CHANNEL_SECRET,
   LINE_CHANNEL_ACCESS_TOKEN,
 } = process.env
@@ -94,6 +95,60 @@ const getImage = async (req, res, next) => {
     .catch((err) => res.status(500).end())
 }
 
+const pushMessage = async (req, res) => {
+  const { message } = req.body
+  const { url, w, h } = getUrl(message)
+  if (!url) return Promise.resolve(null)
+
+  const uuid = uuidv4()
+  const fileName = `stock/${new Date().getTime()}.png`
+  const bucketFile = bucket.file(fileName)
+  const bucketStream = bucketFile.createWriteStream({
+    metadata: {
+      metadata: {
+        firebaseStorageDownloadTokens: uuid,
+      },
+    },
+  })
+
+  bucketStream.on('error', (err) => {
+    next(err)
+  })
+
+  bucketStream.on('finish', () => {
+    const fileLink = getFileLink(STORAGE_BUCKET, fileName, uuid)
+    axois
+      .post(
+        'https://api.line.me/v2/bot/message/multicast',
+        {
+          to: [LINE_USER_ID],
+          messages: [
+            {
+              type: 'image',
+              previewImageUrl: fileLink,
+              originalContentUrl: fileLink,
+            },
+          ],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer {${LINE_CHANNEL_ACCESS_TOKEN}}`,
+          },
+        }
+      )
+      .then(() => {
+        res.json(null)
+      })
+      .catch(() => {
+        res.status(500).end()
+      })
+  })
+
+  const buffer = await puppeteerController.getScreenshot(url, w, h)
+  bucketStream.end(buffer)
+}
+
 const getUrl = (text = '') => {
   const list = text.split('.')
   if (list.length !== 2) return { url: null }
@@ -103,43 +158,30 @@ const getUrl = (text = '') => {
   const day = dayjs().add(-7, 'day').format('YYYY-MM-DD')
   switch (key) {
     case 'fmc':
-    case `${LINE_NOTIFY_NAME} fmc`:
       return { url: `https://moneydj.emega.com.tw/z/ze/zej/zej.djhtm?A=EV000060&B=${day}&C=2` }
     case 'exd':
-    case `${LINE_NOTIFY_NAME} exd`:
       return { url: `https://moneydj.emega.com.tw/z/ze/zej/zej.djhtm?A=EV000020&B=${day}&C=2` }
     case 'wo':
-    case `${LINE_NOTIFY_NAME} wo`:
       return { url: `https://histock.tw/stock/option.aspx?m=week`, w: 1100, h: 1400 }
     case 'rf':
-    case `${LINE_NOTIFY_NAME} rf`:
       return { url: `https://concords.moneydj.com/Z/ZG/ZGK_D.djhtm`, w: 850 }
     case 'ri':
-    case `${LINE_NOTIFY_NAME} ri`:
       return { url: `https://concords.moneydj.com/Z/ZG/ZGK_DD.djhtm`, w: 850 }
     case 'rs':
-    case `${LINE_NOTIFY_NAME} rs`:
       return { url: `https://concords.moneydj.com/Z/ZG/ZGK_DB.djhtm`, w: 850 }
     case 'rm':
-    case `${LINE_NOTIFY_NAME} rm`:
       return { url: `https://concords.moneydj.com/Z/ZG/ZGK_F.djhtm`, w: 850 }
     case 'o':
-    case `${LINE_NOTIFY_NAME} o`:
       return { url: `https://invest.cnyes.com/twstock/TWS/${num}` }
     case 'h':
-    case `${LINE_NOTIFY_NAME} h`:
       return { url: `https://invest.cnyes.com/twstock/TWS/${num}/history` }
     case 'i':
-    case `${LINE_NOTIFY_NAME} i`:
       return { url: `https://invest.cnyes.com/twstock/TWS/${num}/holders/institution` }
     case 'f':
-    case `${LINE_NOTIFY_NAME} f`:
       return { url: `https://invest.cnyes.com/twstock/TWS/${num}/finirating` }
     case 'd':
-    case `${LINE_NOTIFY_NAME} d`:
       return { url: `https://invest.cnyes.com/twstock/TWS/${num}/dividend` }
     case 'p':
-    case `${LINE_NOTIFY_NAME} p`:
       return { url: `https://invest.cnyes.com/twstock/TWS/${num}/profile` }
     default:
       return { url: null }
@@ -156,4 +198,5 @@ module.exports = {
   info,
   echo,
   getImage,
+  pushMessage,
 }
--- routes/api/line.js
+++ routes/api/line.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
+const express = require('express')
 const router = require('express').Router()
 const lineController = require('../../controllers/line.controller')
 
 router.get('/', lineController.info)
 
 router.post('/webhook', lineController.middleware, lineController.getImage)
+
+router.post('/message', express.json(), lineController.pushMessage)
 
 module.exports = router
